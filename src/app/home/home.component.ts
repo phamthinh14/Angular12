fcd8c8534324e92154eb462c288002739703180f
phamthinh14 <thinhpham0717@gmail.com>
2022-03-30 23:27:08 -0400

    Learn about  Data Binding - Events binding - ngModel - Two-way binding

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
+  public name = 'Thinh';
+  public age = 15;
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  public resetName(): void {
+    console.log('resetName');
+    this.name = '';
   }
 
 }
